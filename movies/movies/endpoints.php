59cd359ab29f1ad895940a2f9f7d3dfbaaa15d0a
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-09-18 11:04:21 +0100

    fix: adding endpoints

---
+++ movies/movies/endpoints.php
@@ -0,0 +1,17 @@
+<?php
+
+use src\Controller\MovieController;
+
+$app->get('v1/movies', MovieController::class . ':get');
+
+$app->post('v1/movies', MovieController::class . ':post');
+
+$app->put('v1/movies/{uid}', MovieController::class . ':put');
+
+$app->delete('v1/movies/{uid}', MovieController::class . ':delete');
+
+$app->patch('v1/movies/{uid}', MovieController::class . ':patch');
+
+$app->get('v1/movies/{numberPerPage}', MovieController::class . ':getSelection');
+
+$app->get('v1/movies/{numberPerPage}/sort/{fieldToSort}', MovieController::class . ':getSortedSelection');
--- movies/public/index.php
+++ movies/public/index.php
@@ -3,7 +3,6 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-use src\Controller\MovieController;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -15,38 +14,8 @@ $app = AppFactory::create();
 // Add Error Middleware
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
-// // Define Routes/Endpoints and Middleware
-// $app->get('v1/movies', MovieController::class . ':get');
-
-// $app->post('v1/movies', MovieController::class . ':post');
-
-// $app->put('v1/movies/{uid}', MovieController::class . ':put');
-
-// $app->delete('v1/movies/{uid}', MovieController::class . ':delete');
-
-// $app->patch('v1/movies/{uid}', MovieController::class . ':patch');
-
-// $app->get('v1/movies/{numberPerPage}', MovieController::class . ':getSelection');
-
-// $app->get('v1/movies/{numberPerPage}/sort/{fieldToSort}', MovieController::class . ':getSortedSelection');
-
-$app->get('v1/movies', function (Request $request, Response $response, $args) {
-    $resource = MovieController::class . 'get';
-    $response->getBody()->write(json_encode($resource, JSON_PRETTY_PRINT));
-
-    return $response
-        ->withHeader('Content-Type', 'application/json; charset=UTF-8')
-        ->withStatus(200);
-});
-
-$app->post('v1/movies', function (Request $request, Response $response, $args) {
-    $resource = MovieController::class . 'post';
-    $response->getBody()->write(json_encode($resource, JSON_PRETTY_PRINT));
-
-    return $response
-        ->withHeader('Content-Type', 'application/json; charset=UTF-8')
-        ->withStatus(201);
-});
+// Define Routes/Endpoints and Middleware
+require __DIR__ . '/../movies/endpoints.php';
 
 
 $app->run();
