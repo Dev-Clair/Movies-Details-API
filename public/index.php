a41f8113c154614ab3be3fee3c1dbfa8cdacaa8c
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-09-22 16:51:24 +0100

    feat|fix: modified Controllers and Routes

--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -13,29 +11,6 @@ $app->addRoutingMiddleware();
 
 // Add Error Middleware
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
-
-// Define a route/end-point for API Welcome info
-$app->get('/v1', function (Request $request, Response $response) {
-    $welcome_message = [
-        'message' => "Hello there!, Welcome to movies_detail API",
-        'status' => 'Active',
-        'endpoints' => [
-            'GET: /v1/movies' => 'Retrieves all available resources (movies)',
-            'POST: /v1/movies' => 'Creates a resource (movie) on the server',
-            'PUT: /v1/movies/{uid}' => 'Modifies an entire resource (movie) based on provided resource uid',
-            'DELETE: /v1/movies/{uid}' => 'Deletes or removes resource (movie) based on provided resource uid',
-            'PATCH: /v1/movies/{uid}' => 'Modifies specified parts of a resource (movie) based on provided resource uid',
-            'GET: /v1/movies/{numberPerPage}' => 'Retrieves resources (movies) based on supplied query param {numberPerPage}',
-            'GET: /v1/movies/{numberPerPage}/sort/{fieldToSort}' => 'Retrieves sorted resources (movies) based on supplied query params: {numberPerPage} and {fieldToSort}',
-            'GET: /v1/movies/search/{title}' => 'Retrieves similar resources (movies) based on supplied search string'
-        ]
-    ];
-
-    $response->getBody()->write(json_encode($welcome_message, JSON_PRETTY_PRINT));
-    return $response
-        ->withHeader('Content-Type', 'application/json; charset=UTF-8')
-        ->withStatus(200);
-});
 
 // Define routes/endpoints and middlewares for crud operations
 require __DIR__ . '/../movies.php';
--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -56,6 +56,34 @@ class MovieController extends AbsController
 
 
     /**
+     * Handles GET request for api infor
+     * 
+     */
+    public function getAPIInfo(Request $request, Response $response, array $args): Response
+    {
+        $welcome_message = [
+            'message' => "Hello there!, Welcome to movies_detail API",
+            'status' => 'Active',
+            'endpoints' => [
+                'GET: /v1/movies' => 'Retrieves all available resources (movies)',
+                'POST: /v1/movies' => 'Creates a resource (movie) on the server',
+                'PUT: /v1/movies/{uid}' => 'Modifies an entire resource (movie) based on provided resource uid',
+                'DELETE: /v1/movies/{uid}' => 'Deletes or removes resource (movie) based on provided resource uid',
+                'PATCH: /v1/movies/{uid}' => 'Modifies specified parts of a resource (movie) based on provided resource uid',
+                'GET: /v1/movies/{numberPerPage}' => 'Retrieves resources (movies) based on supplied query param {numberPerPage}',
+                'GET: /v1/movies/{numberPerPage}/sort/{fieldToSort}' => 'Retrieves sorted resources (movies) based on supplied query params: {numberPerPage} and {fieldToSort}',
+                'GET: /v1/movies/search/{title}' => 'Retrieves similar resources (movies) based on supplied search string'
+            ]
+        ];
+
+        $response->getBody()->write(json_encode($welcome_message, JSON_PRETTY_PRINT));
+        return $response
+            ->withHeader('Content-Type', 'application/json; charset=UTF-8')
+            ->withStatus(200);
+    }
+
+
+    /**
      * Handles GET request for fetching movies
      * 
      */
@@ -279,6 +307,10 @@ class MovieController extends AbsController
     }
 
 
+    /**
+     * Handles GET request for searching movies
+     * 
+     */
     public function getSearch(Request $request, Response $response, array $args): Response
     {
         $title =  (string) $args['title'];
