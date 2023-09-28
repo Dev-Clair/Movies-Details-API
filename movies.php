164cf45a49b38045fcb07f1772f6149b58f8bba4
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-09-28 09:22:15 +0100

    bug: debugging log MiddleWares

--- movies.php
+++ movies.php
@@ -8,40 +8,47 @@ use src\Middleware\ResponseLogMiddleWare;
 
 $app->get('/v1', MovieController::class . ':getAPIInfo')
     ->add(new RequestLogMiddleWare)
-    ->add(new ResponseLogMiddleWare);
+    // ->add(new ResponseLogMiddleWare)
+;
 
 $app->get('/v1/movies', MovieController::class . ':get')
     ->add(new RequestLogMiddleWare)
     ->add(new MethodTypeMiddleware(["GET", "POST"]))
-    ->add(new ResponseLogMiddleWare);
+    // ->add(new ResponseLogMiddleWare)
+;
 
 $app->post('/v1/movies', MovieController::class . ':post')
     ->add(new RequestLogMiddleWare)
     ->add(new MethodTypeMiddleware(["GET", "POST"]))
     ->add(new ContentTypeMiddleware)
-    ->add(new ResponseLogMiddleWare);
+    // ->add(new ResponseLogMiddleWare)
+;
 
 $app->put('/v1/movies/{uid}', MovieController::class . ':put')
     ->add(new RequestLogMiddleWare)
     ->add(new MethodTypeMiddleware(["PUT", "DELETE", "PATCH"]))
     ->add(new ContentTypeMiddleware)
-    ->add(new ResponseLogMiddleWare);
+    // ->add(new ResponseLogMiddleWare)
+;
 
 $app->patch('/v1/movies/{uid}', MovieController::class . ':patch')
     ->add(new RequestLogMiddleWare)
     ->add(new MethodTypeMiddleware(["PUT", "DELETE", "PATCH"]))
     ->add(new ContentTypeMiddleware)
-    ->add(new ResponseLogMiddleWare);
+    // ->add(new ResponseLogMiddleWare)
+;
 
 $app->delete('/v1/movies/{uid}', MovieController::class . ':delete')
     ->add(new RequestLogMiddleWare)
     ->add(new MethodTypeMiddleware(["PUT", "DELETE", "PATCH"]))
-    ->add(new ResponseLogMiddleWare);
+    // ->add(new ResponseLogMiddleWare)
+;
 
 $app->get('/v1/movies/{numberPerPage}', MovieController::class . ':getSelection')
     ->add(new RequestLogMiddleWare)
     ->add(new MethodTypeMiddleware(["GET"]))
-    ->add(new ResponseLogMiddleWare);
+    // ->add(new ResponseLogMiddleWare)
+;
 
 $app->get('/v1/movies/{numberPerPage}/sort/{fieldToSort}', MovieController::class . ':getSortedSelection')
     ->add(new RequestLogMiddleWare)
--- src/Trait/LogToFileMiddleWareTrait.php
+++ src/Trait/LogToFileMiddleWareTrait.php
@@ -9,7 +9,7 @@ trait LogToFileMiddleWareTrait
     public function logToFile(array $data): void
     {
         $logFilePath = __DIR__ . '/../../logs/app.log';
-        $logData = $data;
+        $logData = json_encode($data, JSON_PRETTY_PRINT) . PHP_EOL;
 
         file_put_contents($logFilePath, $logData, FILE_APPEND);
     }
