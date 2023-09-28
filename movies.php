b63b45d2e3e0e6913d6cb76635c554bc3d8c6ab9
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-09-28 11:13:35 +0100

    fix|bug: fixed bug in routes

--- movies.php
+++ movies.php
@@ -7,54 +7,48 @@ use src\Middleware\RequestLogMiddleWare;
 use src\Middleware\ResponseLogMiddleWare;
 
 $app->get('/v1', MovieController::class . ':getAPIInfo')
-    ->add(new RequestLogMiddleWare)
-    ->add(new ResponseLogMiddleWare);
+    ->add(new ResponseLogMiddleWare)
+    ->add(new RequestLogMiddleWare);
 
 $app->get('/v1/movies', MovieController::class . ':get')
-    ->add(new RequestLogMiddleWare)
     ->add(new MethodTypeMiddleware(["GET", "POST"]))
-    // ->add(new ResponseLogMiddleWare)
-;
+    ->add(new ResponseLogMiddleWare)
+    ->add(new RequestLogMiddleWare);
 
 $app->post('/v1/movies', MovieController::class . ':post')
-    ->add(new RequestLogMiddleWare)
     ->add(new MethodTypeMiddleware(["GET", "POST"]))
     ->add(new ContentTypeMiddleware)
-    // ->add(new ResponseLogMiddleWare)
-;
+    ->add(new ResponseLogMiddleWare)
+    ->add(new RequestLogMiddleWare);
 
 $app->put('/v1/movies/{uid}', MovieController::class . ':put')
-    ->add(new RequestLogMiddleWare)
     ->add(new MethodTypeMiddleware(["PUT", "DELETE", "PATCH"]))
     ->add(new ContentTypeMiddleware)
-    // ->add(new ResponseLogMiddleWare)
-;
+    ->add(new ResponseLogMiddleWare)
+    ->add(new RequestLogMiddleWare);
 
 $app->patch('/v1/movies/{uid}', MovieController::class . ':patch')
-    ->add(new RequestLogMiddleWare)
     ->add(new MethodTypeMiddleware(["PUT", "DELETE", "PATCH"]))
     ->add(new ContentTypeMiddleware)
-    // ->add(new ResponseLogMiddleWare)
-;
+    ->add(new ResponseLogMiddleWare)
+    ->add(new RequestLogMiddleWare);
 
 $app->delete('/v1/movies/{uid}', MovieController::class . ':delete')
-    ->add(new RequestLogMiddleWare)
     ->add(new MethodTypeMiddleware(["PUT", "DELETE", "PATCH"]))
-    // ->add(new ResponseLogMiddleWare)
-;
+    ->add(new ResponseLogMiddleWare)
+    ->add(new RequestLogMiddleWare);
 
 $app->get('/v1/movies/{numberPerPage}', MovieController::class . ':getSelection')
-    ->add(new RequestLogMiddleWare)
     ->add(new MethodTypeMiddleware(["GET"]))
-    // ->add(new ResponseLogMiddleWare)
-;
+    ->add(new ResponseLogMiddleWare)
+    ->add(new RequestLogMiddleWare);
 
 $app->get('/v1/movies/{numberPerPage}/sort/{fieldToSort}', MovieController::class . ':getSortedSelection')
-    ->add(new RequestLogMiddleWare)
     ->add(new MethodTypeMiddleware(["GET"]))
-    ->add(new ResponseLogMiddleWare);
+    ->add(new ResponseLogMiddleWare)
+    ->add(new RequestLogMiddleWare);
 
 $app->get('/v1/movies/search/{title}', MovieController::class . ':getSearch')
-    ->add(new RequestLogMiddleWare)
     ->add(new MethodTypeMiddleware(["GET"]))
-    ->add(new ResponseLogMiddleWare);
+    ->add(new ResponseLogMiddleWare)
+    ->add(new RequestLogMiddleWare);
